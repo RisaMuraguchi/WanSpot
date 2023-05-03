feadd579e7e63d07a0012a1410ba55ae09f7b17e
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-03 07:38:08 +0000

    controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
   # devise関係のルーティング後で直す
   devise_for :admins
   devise_for :users

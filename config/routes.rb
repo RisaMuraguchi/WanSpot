0517ae0aea714dcb38dd3bf42f02558d4fdd0144
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-03 14:16:13 +0000

    新規登録画面レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # devise関係のルーティング後で直す
   devise_for :admins
-  
+
   devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'

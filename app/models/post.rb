e3d9e48e667275227c95680f9e49e87912d2bcfd
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-03 11:27:56 +0000

    bootstrap/fontawesome

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/index'
+
   # devise関係のルーティング後で直す
   devise_for :admins
-  devise_for :users
+  
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  # devise_for :users
+
+  root :to =>"homes#top"
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
+  end
+
+resources :users, only: [:index,:show,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+end
+
+
 end

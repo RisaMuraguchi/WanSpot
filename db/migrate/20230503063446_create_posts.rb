1aea699a970848440c25a5921b7a64b8acefa4cb
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-03 07:23:29 +0000

    モデル作成

---
+++ db/migrate/20230503063446_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.text :caption
+      t.string :address
+      t.float :lat
+      t.float :lng
+      t.timestamps
+    end
+  end
+end

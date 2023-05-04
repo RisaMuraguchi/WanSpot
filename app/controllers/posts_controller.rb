e62c0fc38705473476cfdf968127f568e74b5950
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-04 06:55:59 +0000

    user controller編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,43 @@
 class PostsController < ApplicationController
+
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
+    @user = current_user
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @comment = PostComment.new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: "You have updated post successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @user = current_user
+  end
+
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def index
+  def update
+    @user = User.find(params[:id])
+    if  @user.update(user_params)
+        redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+        render :edit
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile, :profile_image)
+  end
+
 end

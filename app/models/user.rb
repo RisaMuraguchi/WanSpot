3ec87d5ea51178bf3672b6d8fbdfe44082c34593
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-07 13:03:30 +0000

    ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,5 +40,13 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+  # ゲストログイン用
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  # devise_for :users
+  
+  # ゲストログイン用
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   root :to =>"homes#top"
 

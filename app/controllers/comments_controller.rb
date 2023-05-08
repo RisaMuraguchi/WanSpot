0c282a161e7751e3aa96e06f07557900adfaaf92
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-08 01:54:14 +0000

    コメント機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,19 +3,20 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.id = post.id
+    comment.post_id = post.id
     comment.save
     redirect_to request.referer
   end
+  
 
   def destroy
     Comment.find(params[:id]).destroy
     redirect_to request.referer
   end
 
-  privates
+  private
 
-  def book_comment_params
+  def comment_params
     params.require(:comment).permit(:comment)
   end
 

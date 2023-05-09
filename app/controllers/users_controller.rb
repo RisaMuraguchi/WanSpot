f3dc3394f14fa53ac0fb30b7688a44f9845edcf1
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-05-09 14:08:16 +0000

    いいね一覧修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_guest_user, only: [:edit]
-  before_action :set_user, only: [:likes]
+  # before_action :set_user, only: [:show, :edit, :update, :likes]
 
   def index
     @users = User.all
@@ -27,8 +27,12 @@ class UsersController < ApplicationController
   end
 
   def likes
+    @user = User.find(params[:id])
     likes = Like.where(user_id: @user.id).pluck(:post_id)
     @like_posts = Post.find(likes)
+    if @like_posts.empty?
+      @message = "いいねした投稿はありません"
+    end
   end
 
   private
@@ -44,8 +48,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
 end
